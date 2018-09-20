fb655f801ca4fdd39b79d8d64ea1960a72801b95
luisfelippefloriani <felippefloriani@gmail.com>
2018-09-20 14:28:05 -0300

    Initi

---
+++ webservice/src/br/com/felippefloriani/service/ProdutoService.java
@@ -0,0 +1,43 @@
+package br.com.felippefloriani.service;
+
+
+import java.util.List;
+
+import br.com.felippefloriani.model.dao.ProdutoDAO;
+import br.com.felippefloriani.model.domain.Produto;
+
+public class ProdutoService {
+
+	private ProdutoDAO dao = new ProdutoDAO();
+    
+    public List<Produto> getProdutos() {
+        return dao.getAll();
+    }
+     
+    public Produto getProduto(Long id) {
+        return dao.getById(id);
+    }
+     
+    public Produto saveProduto(Produto produto) {
+        return dao.save(produto);
+    }
+     
+    public Produto updateProduto(Produto produto) {
+        return dao.update(produto);
+    }
+     
+    public Produto deleteProduto(Long id) {
+        return dao.delete(id);
+    }
+     
+    public List<Produto> getProdutosByPagination(int firstResult, int maxResults) {
+        return dao.getByPagination(firstResult, maxResults);
+    }
+     
+    public List<Produto> getProdutoByName(String name) {
+        return dao.getByName(name);
+    }
+	
+	
+	
+}
